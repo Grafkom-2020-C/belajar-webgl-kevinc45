4bce03059378bb5ca9d3ca18db27940cf83dac43
KC <kevin45christian@gmail.com>
2020-10-15 15:03:22 +0700

    Drawing Simple Triangle Using 'gl.LINES'

--- pertemuan-3/main.js
+++ pertemuan-3/main.js
@@ -1,47 +1,72 @@
 function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
-  
+    
+    var vertices = [
+        0.0, 0.5,       //Titik A
+        0.5, -0.5,      //Titik B
+        0.5, -0.5,      //Titik B
+        -0.5, -0.5,     //Titik C
+        -0.5, -0.5,     //Titik C
+        0.0, 0.5        //Titik A
+    ];
+    
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    
     // Ibaratnya di bawah ini adalah .c
     var vertexShaderSource = `
-      void main() {
+    attribute vec2 a_Position;
+    void main() {
         gl_PointSize = 25.0;
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-      }
+        gl_Position = vec4(a_Position, 0.0, 1.0);
+    }
     `;
+    
     var fragmentShaderSource = `
-      void main() {
+    void main() {
         gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
-      }
+    }
     `;
-  
+    
     // Ibaratnya di bawah ini adalah .o
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-  
+    
     // Ibarat mengetikkan teks source code ke dalam penampung .c
     gl.shaderSource(vertexShader, vertexShaderSource);
     gl.shaderSource(fragmentShader, fragmentShaderSource);
-  
+    
     // Ibarat mengompilasi .c menjadi .o
     gl.compileShader(vertexShader);
     gl.compileShader(fragmentShader);
-  
+    
     // Ibarat membuatkan penampung .exe
     var shaderProgram = gl.createProgram();
-  
+    
     // Ibarat memasukkan "adonan" .o ke dalam penampung .exe
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
-  
+    
     // Ibarat menggabung-gabungkan "adonan" yang ada di dalam penampung .exe
     gl.linkProgram(shaderProgram);
-  
+    
     // Ibarat memulai menggunakan "cat" .exe ke dalam konteks grafika (penggambaran)
     gl.useProgram(shaderProgram);
-  
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    
+    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+    
     gl.clearColor(1, 0.87, 0.35, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-  
-    gl.drawArrays(gl.POINTS, 0, 1);
-  }
+    
+    var primitive = gl.LINES;
+    var offset = 0;
+    var nVertex = 6;
+    gl.drawArrays(primitive, offset, nVertex);
+}
